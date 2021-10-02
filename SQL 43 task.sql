485deb4c999816eef0d67111d8994030f8649467
Gamenyuk Stanislav <stangamenuk@gmail.com>
2021-10-03 00:32:28 +0500

    Create SQL 43 task.sql

---
+++ SQL 43 task.sql
@@ -0,0 +1,8 @@
+/*Укажите сражения, которые произошли в годы, не совпадающие ни с одним из годов спуска кораблей на воду.*/
+
+SELECT name
+FROM Battles
+WHERE year (date) NOT IN 
+ (SELECT launched
+  FROM Ships
+  WHERE launched IS NOT NULL)
